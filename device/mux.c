720e28952db373cfc47bea8235a1bb0ae3f9b3c0
Unknown <elderdtramontin@gmail.com>
2018-10-01 17:01:56 -0300

    Add files to deal with MUX/DEMUX

--- device/mux.c
+++ device/mux.c
@@ -4,4 +4,26 @@
  *  Created on: 4 de set de 2018
  *      Author: elder
  */
+#include "mux.h"
+#include "../hal.h"
 
+void mux_setup(void)
+{
+    MUX_SELECT_OUTPUT(HIGH);
+    MUX_SELECT_SETUP();
+
+    MUX_OUTPUT_ENABLE_N_OUTPUT(LOW);
+    MUX_OUTPUT_ENABLE_N_SETUP();
+}
+
+void mux_select(uint8_t selected_device)
+{
+    if(selected_device == SELECT_HUMAN)
+    {
+        MUX_SELECT_OUTPUT(LOW);
+    }
+    else
+    {
+        MUX_SELECT_OUTPUT(HIGH);
+    }
+}
--- device/mux.h
+++ device/mux.h
@@ -7,8 +7,12 @@
 #ifndef DEVICE_MUX_H_
 #define DEVICE_MUX_H_
 
+#include <stdint.h>
 
+#define SELECT_BRAVE    0
+#define SELECT_HUMAN    1
 
-
+void mux_setup(void);
+void mux_select(uint8_t selected_device);
 
 #endif /* DEVICE_MUX_H_ */
